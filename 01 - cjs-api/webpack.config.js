178122ae49b92dfe0bd2c094b19eea3daaf1bab3
Clay Curry <me@claycurry.com>
2024-01-17 15:15:34 -0600

    saving tutorial 1

---
+++ 01 - cjs-api/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        app: './src/app.js',
+        main: {
+            dependOn: 'app',
+            import: './src/index.js'
+        }
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Webpack App',
+            filename: 'index.html',
+            template: 'src/template.html'
+        })
+    ]
+};
